fb530d6f32bc82e4a1cb61bf473afef229b0c507
e5aka <874050707@qq.com>
2018-04-20 20:53:03 +0800

    20180420

---
+++ stack/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main(){
+    int a = 3;
+    int *b = &a;
+    int *c, *d;
+    *c = a;
+    d = &a;
+    printf("Into the night\n");
+    scanf("%d",b);
+    printf("the night");
+}
