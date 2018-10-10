a6c714d17f93ed361ccc11c9dca683d869107fe2
e5aka <874050707@qq.com>
2018-10-10 18:44:10 +0800

    stack

---
+++ wangdao/chapter3/stack/2die4.c
@@ -0,0 +1,33 @@
+#define MaxSize 10
+typedef int ElemType;
+
+#include<stdio.h>
+#include<stdlib.h>
+#include "SqStack.h"
+
+int main(int argc, char *argv[]) {
+	SqStack mystack;
+    printf("The max stacksize is %d\n", sizeof(mystack));
+	int x, i = 0, N;
+    
+    printf("N = ");
+	scanf("%d", &N);
+                                        
+	initStack(&mystack);
+    if(N == 0)
+		stackPush(&mystack, i);	
+    while(N != 0){	
+        i = N%2;
+        N = N/2;
+        stackPush(&mystack, i);
+    }
+	printf("Out of stace queue:\n");
+	while(!stackEmpty(mystack)) 
+	{
+		stackPop(&mystack, &x);
+		printf("%d", x);
+	}
+	printf("\n");
+
+    return 1;
+}
